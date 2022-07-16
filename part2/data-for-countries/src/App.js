11e8c26d01bb39ef81eacd5a27e92f1f92d8b125
Karabo <karaborabothata0@gmail.com>
2022-07-16 22:09:00 +0200

    display - view filtered country's information

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,34 +1,61 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 const App = () => {
-	
-	const [countriesData, setCountriesData] = useState([])
-	const [filterTerm, setFilterTerm] = useState('')
+	const [countriesData, setCountriesData] = useState([]);
+	const [filterTerm, setFilterTerm] = useState("");
 
-	const queryMessage = 'Too many matches, make search query more specific'
-	const handleFilterTerm = (e) => {setFilterTerm(e.target.value)}
-	const displayCountries = provideFilteredCountries()
+	const queryMessage = "Too many matches, make search query more specific";
+	const handleFilterTerm = (e) => {
+		setFilterTerm(e.target.value);
+	};
+	const displayCountries = provideFilteredCountries();
+	const languagesInCountry = provideLanguages()
 
-	console.log(displayCountries.length);
-
-	function provideFilteredCountries(){
-		return countriesData.filter(item=>item.name.common.toLowerCase().includes(filterTerm))
+	function provideFilteredCountries() {
+		return countriesData.filter((item) =>
+			item.name.common.toLowerCase().includes(filterTerm),
+		);
 	}
 
-  useEffect(() => {
-    axios
-      .get('https://restcountries.com/v3.1/all')
-      .then(response => {
-        setCountriesData(response.data)
-      })
-  }, [])
+	function provideLanguages() {
+		let arrayOfLanguages = [];
+			const currentCountryLanguages = displayCountries[0].languages
+			for(const key of Object.keys(currentCountryLanguages)){
+				arrayOfLanguages.push(currentCountryLanguages[key])
+			}
+	return arrayOfLanguages
+	}
 
-	return <div>
-		<h1>Find Country <input value={filterTerm} onChange={handleFilterTerm}/></h1>
-		{(displayCountries.length > 10 && filterTerm) && <h3>{queryMessage}</h3>}
-		{displayCountries.map(country=><li>{country.name.common}</li>)}
-	</div>;
+	useEffect(() => {
+		axios.get("https://restcountries.com/v3.1/all").then((response) => {
+			setCountriesData(response.data);
+		});
+	}, []);
+
+	return (
+		<div>
+			<h1>
+				Find Country <input value={filterTerm} onChange={handleFilterTerm} />
+			</h1>
+			{displayCountries.length > 10 && filterTerm && <h3>{queryMessage}</h3>}
+			{displayCountries.map((country) => (
+				<li>{country.name.common}</li>
+			))}
+			{displayCountries.length === 1 &&
+				displayCountries.map((country) => {
+					return (
+						<>
+							<h1>{country.name.common}</h1>
+							<p>Capital city: {country.capital}</p>
+							<p>Area: {country.area}</p>
+							<h3>Languages</h3>
+							{languagesInCountry.map(language=><li>{language}</li>)}
+						</>
+					);
+				})}
+		</div>
+	);
 };
 
 export default App;
