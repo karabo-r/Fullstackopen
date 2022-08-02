c23fb4e541a6767af810eb42f5dc96aa8bcb56ab
Karabo <karaborabothata0@gmail.com>
2022-08-02 14:19:57 +0200

    exercise 5.2 complete

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -35,6 +35,11 @@ const App = () => {
 		setUsername(e.target.value);
 	}
 
+	function handleLogout(){
+		localStorage.clear()
+		setIsLoggedIn(false)
+	}
+
 	// no login required if there's already a user in localstorage
 
 	useEffect(() => {
@@ -60,7 +65,7 @@ const App = () => {
 			) : (
 				<>
 					<h2>blogs</h2>
-					<h3>{user.username} has been logged in</h3>
+					<h3>{user.username} has been logged in <button onClick={handleLogout}>logout</button></h3> 
 					{blogs.map((blog) => (
 						<Blog key={blog.id} blog={blog} />
 					))}
