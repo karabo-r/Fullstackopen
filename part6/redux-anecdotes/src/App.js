be8e1cef4a0dbf39bb79d1810e556f5d395b19d8
Karabo <karaborabothata0@gmail.com>
2022-09-26 12:01:08 -0300

    exercise 6.4 complete

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,34 +1,44 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { asObject } from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+	const anecdotes = useSelector((state) => state);
+	const dispatch = useDispatch();
 
-  const vote = (id) => {
-    dispatch({id, type: 'addVote'})
-    }
+	const inputRef = useRef(null);
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
+	const vote = (id) => {
+		dispatch({ id, type: "addVote" });
+	};
 
-export default App
+	const create = (e) => {
+		e.preventDefault();
+		const content = inputRef.current.value;
+		dispatch({ type: "create", data: asObject(content) });
+	};
+
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => vote(anecdote.id)}>vote</button>
+					</div>
+				</div>
+			))}
+			<h2>create new</h2>
+			<form onSubmit={(e) => create(e)}>
+				<div>
+					<input ref={inputRef} />
+				</div>
+				<button type="submit">create</button>
+			</form>
+		</div>
+	);
+};
+
+export default App;
