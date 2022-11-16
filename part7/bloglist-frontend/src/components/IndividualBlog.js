51b211477390e107f0b165b3d7a3477f7bbe59aa
Karabo <karaborabothata0@gmail.com>
2022-11-16 12:11:08 -0400

    exercise 7.18 complete

--- part7/bloglist-frontend/src/components/IndividualBlog.js
+++ part7/bloglist-frontend/src/components/IndividualBlog.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import UserServices from "../services/users";
 import CurrentUser from "./CurrentUser";
+import Navigation from "./Navigation";
 const IndividualBlog = () => {
 	const [blogDetails, setBlogDetails] = useState([]);
 	// who added the blog
@@ -12,28 +13,42 @@ const IndividualBlog = () => {
 
 	const { id } = useParams();
 
+	const processedBlog = blogDetails?.map((item) => {
+		return item.blogs?.map((blog) => {
+			// console.log(blog);
+			if (blog.id === id) {
+				return (
+					<div key={blog.id}>
+						<h1>
+							{blog.title} {blog.author}
+						</h1>
+						<a href={blog.url}>{blog.url}</a>
+
+						<p>
+							{blog.likes ? blog.likes : 0} likes <br />
+							Added by {item.name}
+						</p>
+					</div>
+				);
+			}
+		});
+	});
+
+	const processedComments = blogDetails?.map((item) => {
+		return item.blogs?.map((blog) => {
+			if (blog.id === id) {
+				return blog.comments.map((comment) => <li>{comment}</li>);
+			}
+		});
+	});
+
 	return (
 		<>
+			<Navigation />
 			<CurrentUser />
-			{blogDetails?.map((item) => {
-				return item.blogs?.map((blog) => {
-					if (blog.id === id) {
-						return (
-							<div key={blog.id}>
-								<h1>
-									{blog.title} {blog.author}
-								</h1>
-								<a href={blog.url}>{blog.url}</a>
-
-								<p>
-									{blog.likes ? blog.likes : 0} likes <br />
-									Added by {item.name}
-								</p>
-							</div>
-						);
-					}
-				});
-			})}
+			{processedBlog}
+			<h2>comments</h2>
+			{processedComments}
 		</>
 	);
 };
